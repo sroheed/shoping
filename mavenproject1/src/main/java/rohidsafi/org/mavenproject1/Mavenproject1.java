87abebd2e7ddf0a12a0697734af86d1ea4deeb44
Mohammad Rohid safi <sroheed@gmail.com>
2022-07-18 22:35:35 +0200

    added project files

---
+++ mavenproject1/src/main/java/rohidsafi/org/mavenproject1/Mavenproject1.java
@@ -0,0 +1,13 @@
+
+package rohidsafi.org.mavenproject1;
+
+/**
+ *
+ * @author rohidsafi
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
